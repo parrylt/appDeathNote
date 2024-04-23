510dd17cedd8528021a34e59716eff043869e755
Cristiandiv <132522844+Cristiandiv@users.noreply.github.com>
2024-04-22 22:51:30 -0300

    Create Firebase.js

---
+++ Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZKx5ktLWIQn-In_KCaYyWs0TjO7RjvhY",
+  authDomain: "seriesbd-5f108.firebaseapp.com",
+  projectId: "seriesbd-5f108",
+  storageBucket: "seriesbd-5f108.appspot.com",
+  messagingSenderId: "646230852120",
+  appId: "1:646230852120:web:31ca02138ef3ae7b1bf96d"
+};
+
+const app = initializeApp(firebaseConfig);
+
+////usar o firebase do seriesbd
