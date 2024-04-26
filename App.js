97bf1b380ef6148bf789940ccc9f6c3ec67330fa
Rômulo Junior <132522416+parrylt@users.noreply.github.com>
2024-04-26 20:04:04 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
+import RotasTab from "./pages/rotasTab";
+
+export default function App (){
   return (
-
-<View style={estilo.container}>
-
-<Image source={require('./assets/deathnote.png')}/>
-
-
-</View>
+      <NavigationContainer>
+        <RotasTab/>
+      </NavigationContainer>
 
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
